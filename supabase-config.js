bb333d495e33ba6aaeab2759ace545f51b6c8889
gamemaker7788 <WQC1145142025@outlook.com>
2025-09-06 21:35:45 +0800

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,7 @@
 // 替换为你的Supabase配置
 const supabaseUrl = 'https://fezxhcmiefdbvqmhczut.supabase.co';
-const supabaseKey = 'yp1986213...';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZlenhoY21pZWZkYnZxbWhjenV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcxNjE1MDYsImV4cCI6MjA3MjczNzUwNn0.MdXghSsixHXeYhZKbMYuJGehMUvdbtixGNjMmBPMKKU';
 
 // 创建Supabase客户端
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+
